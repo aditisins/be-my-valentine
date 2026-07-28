90f4b360dd7aa619cd8105ef82a0e63752e3a3ca
Aditi Sinha <145170588+aditisins@users.noreply.github.com>
2026-07-27 18:33:35 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,51 @@
+globalThis.valentineConfig = {
+  pageTitle: "A Very Important Valentine Question",
+  question: "Will you be my Valentine?",
+  shareText: "I said yes!",
+  soundPath: "./sounds/click.mp3",
+  stickerImage: "./images/baddie.jpg",
+
+  yesLabels: [
+    "Yes",
+    "Yes, obviously",
+    "Yes, please",
+    "YES, PLEASE",
+    "YES!!!",
+  ],
+
+  images: [
+    "./images/image1.gif",
+    "./images/image2.gif",
+    "./images/image3.gif",
+    "./images/image4.gif",
+    "./images/image5.gif",
+  ],
+
+  imageAlts: [
+    "A happy duck surrounded by hearts",
+    "A duck holding a heart",
+    "A duck crying dramatically",
+    "A sad duck standing in the rain",
+    "A determined duck making one last appeal",
+  ],
+
+  noSteps: [
+    { label: "No" },
+    { label: "Are you sure?" },
+    { label: "I will cry" },
+    { label: "Ooh, look how much bigger that other button is" },
+    { label: "Say yes or else..." },
+  ],
+
+  success: {
+    title: "It's a date.",
+    image: "./images/image7.gif",
+    imageAlt: "Two ducks celebrating together",
+  },
+
+  decline: {
+    title: "No worries.",
+    image: "./images/default.gif",
+    imageAlt: "A sweet duck holding a heart",
+  },
+};
